430de54f3a06102cfb17e5f3e09f6f52c2b623dd
Martín Araya <martinaraya@gmail.com>
2022-08-01 01:34:13 +0200

    empaquetado y subido a pypi bajo el nombre de unyts

--- units/__init__.py
+++ units/__init__.py
@@ -6,8 +6,8 @@ Created on Sat Oct 24 18:24:20 2020
 @author: martin
 """
 
-__version__ = '0.1.0'
-__release__ = 20220524
+__version__ = '0.1.1'
+__release__ = 20220731
 
 from .units import *
 from ._convert import convertUnit
