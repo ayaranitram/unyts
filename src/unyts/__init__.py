be5b26d8d7e75a688f9bc6acc0fc08c465746615
Martín Araya <martinaraya@gmail.com>
2022-08-20 10:16:07 +0200

    updated for convert2 testing

--- src/unyts/__init__.py
+++ src/unyts/__init__.py
@@ -6,13 +6,14 @@ Created on Sat Oct 24 18:24:20 2020
 @author: Martín Carlos Araya <martinaraya@gmail.com>
 """
 
-__version__ = '0.1.7'
-__release__ = 20220813
+__version__ = '0.2.0'
+__release__ = 20220819
 
 # from .units.custom import *
 from .units.define import units
 from ._convert import convertUnit
 from ._convert import convertUnit as convert
+from ._convert import convertUnit2 as convert2
 from ._database import unitsNetwork
 
 def print_path(switch):
