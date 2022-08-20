ce909ec41645959cc291c03ad6bf49b6de315226
Martín Araya <martinaraya@gmail.com>
2022-08-20 10:13:12 +0200

    added NoCoversionFound error

--- src/unyts/_errors.py
+++ src/unyts/_errors.py
@@ -6,14 +6,20 @@ Created on Sat Oct 24 12:10:14 2020
 @author: Martín Carlos Araya <martinaraya@gmail.com>
 """
 
-__all__ = ['WrongUnits','WrongValue']
-__version__ = '0.0.20-10-24'
+__all__ = ['WrongUnits', 'WrongValue', 'NoConversionFound']
+__version__ = '0.0.2'
+__release__ = 20220819
 
 class WrongUnits(Exception):
-    def __init__(self,message='unit not listed in library, unit must be a string'):
+    def __init__(self, message='unit not listed in library, unit must be a string.'):
         print('ERROR: Wrong Units, ' + message)
 
 
 class WrongValue(Exception):
-    def __init__(self,message='value unit must be a float or integer'):
+    def __init__(self, message='value unit must be a float or integer.'):
         print('ERROR: Wrong Value, ' + message)
+        
+
+class NoConversionFound(Exception):
+    def __init__(self, message='for the provided units.'):
+        print('ERROR: Conversion path not found, ' + message)
