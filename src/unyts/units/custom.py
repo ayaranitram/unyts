901000ae972ec6ed778def3e52f9798753e21b2d
Martín Araya <martinaraya@gmail.com>
2023-01-24 00:18:50 +0100

    implemented method to set custom units and conversions

--- src/unyts/units/custom.py
+++ src/unyts/units/custom.py
@@ -6,9 +6,9 @@ Created on Sat Oct 24 14:34:59 2020
 @author: Martín Carlos Araya <martinaraya@gmail.com>
 """
 
-__version__ = '0.5.2'
-__release__ = 20230118
-__all__ = ['CustomUnits', 'UserUnits', 'OtherUnits']
+__version__ = '0.5.3'
+__release__ = 20230121
+__all__ = ['CustomUnits', 'UserUnits', 'OtherUnits', 'set_unit', 'set_conversion']
 
 from ..dictionaries import dictionary
 from ..unit_class import Unit
@@ -31,6 +31,44 @@ class UserUnits(Unit):
         self.kind = UserUnits
         units = units.strip()
         if units not in dictionary['UserUnits']:
-            dictionary['UserUnits'].append(units)
+            dictionary['UserUnits'] += (units,)
         self.value = self.check_value(value)
         self.unit = self.check_unit(units)
+
+
+def set_unit(unit_name: str) -> bool:
+    from ..database import units_network
+    from ..network import UNode
+    units_network.add_node(UNode(unit_name))
+
+
+def set_conversion(from_units: str, to_units: str, conversion, reverse_conversion=None) -> bool:
+    from ..database import units_network
+    from ..network import UNode, Conversion
+    if reverse_conversion is None:
+        def reverse_conversion(x): return x / conversion(1)
+    if type(from_units) is str:
+        pass
+    elif hasattr(from_units, 'units') and type(from_units.units) is str:
+        from_units = from_units.units
+    else:
+        raise TypeError("`from_units` must be str or Unit.")
+    if type(to_units) is str:
+        pass
+    elif hasattr(to_units, 'units') and type(to_units.units) is str:
+        to_units = to_units.units
+    else:
+        raise TypeError("`to_units` must be str or Unit.")
+    if not hasattr(conversion, '__call__') and hasattr(conversion, '__getitem__'):
+        raise TypeError("`conversion` must be callable.")
+    if not hasattr(reverse_conversion, '__call__') and hasattr(reverse_conversion, '__getitem__'):
+        raise TypeError("`reverse_conversion` must be callable.")
+
+    units_network.add_node(UNode(from_units))
+    units_network.add_node(UNode(to_units))
+    units_network.add_edge(Conversion(units_network.get_node(from_units),
+                                      units_network.get_node(to_units),
+                                      conversion))
+    units_network.add_edge(Conversion(units_network.get_node(to_units),
+                                      units_network.get_node(from_units),
+                                      reverse_conversion))
