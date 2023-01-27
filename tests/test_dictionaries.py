6a33b93c71bdbff8684f00f01bfa2bc8761b567d
Martín Araya <martinaraya@gmail.com>
2023-01-27 20:58:06 +0100

    test dictionary

--- tests/test_dictionaries.py
+++ tests/test_dictionaries.py
@@ -9,9 +9,10 @@ from unyts.dictionaries import dictionary
 from unyts import units
 from string import ascii_uppercase
 
+
 def key2name(txt):
-    return (''.join([('_' if s in ascii_uppercase else '') +  s.lower() for s in txt])).strip('_')
-    
+    return (''.join([('_' if s in ascii_uppercase else '') + s.lower() for s in txt])).strip('_')
+
 
 max_check = 10
 
@@ -20,11 +21,13 @@ for key in dictionary:
         continue
     print(key)
     if len(dictionary[key]) > max_check:
-        to_check = [u for u in dictionary[key] if (u.count('*') <= 1 and u.count('/') == 0) or (u.count('/') <= 1 and u.count('*') == 0)]
+        to_check = [u for u in dictionary[key] if
+                    (u.count('*') <= 1 and u.count('/') == 0) or (u.count('/') <= 1 and u.count('*') == 0)]
     else:
         to_check = dictionary[key]
     for unit in to_check[:max_check]:
-        if key in ['Length', 'Rate'] and unit == 'l' or (len(unit) == 2 and unit.endswith('l')) or ('/' in unit and len(unit.split('/')[0]) <= 2 and unit.split('/')[0].endswith('l')):
+        if key in ['Length', 'Rate'] and unit == 'l' or (len(unit) == 2 and unit.endswith('l')) or (
+                '/' in unit and len(unit.split('/')[0]) <= 2 and unit.split('/')[0].endswith('l')):
             continue  # litre is defines as length for SI sufixes (linear multiplier)
         if unit.lower() in ['ounce', 'oz', 'ounces'] and key == 'Weight':
             continue  # 'ounce' can be volume or mass. It is always instantiated as Volume but ounce can be converted to mass
@@ -33,7 +36,7 @@ for key in dictionary:
         if key == 'Capacitance' and unit == 'F':
             continue  # F for Farad
         rept_units = [k for k in dictionary for u in dictionary[k] if u == unit]
-        if len(rept_units) > 1 and unit not in ['F', 'l', 'ounces', 'OUNCES', 'ounce', 'OUNCE', 'oz'] \
-                and key not in ['PressureGradient', 'Date', 'Impedance']:
-            raise ValueError('unit ' + str(unit) + ' repeated in more than one dictionary:\n ' + '\n '.join(rept_units) )
+        if len(rept_units) > 1 and (unit not in ['F', 'l', 'ounces', 'OUNCES', 'ounce', 'OUNCE', 'oz', 'Ohm']
+                                    or key not in ['PressureGradient', 'Date', 'Impedance']):
+            raise ValueError('unit ' + str(unit) + ' repeated in more than one dictionary:\n ' + '\n '.join(rept_units))
         assert units(1, unit).name.lower() == key2name(key)
