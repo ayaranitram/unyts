0c64c99145e79648ac676673a92906a3af6a203a
Martín Araya <martinaraya@gmail.com>
2023-02-21 08:48:54 +0100

    new version

--- src/unyts/__init__.py
+++ src/unyts/__init__.py
@@ -6,8 +6,8 @@ Created on Sat Oct 24 18:24:20 2020
 @author: Martín Carlos Araya <martinaraya@gmail.com>
 """
 
-__version__ = '0.5.17'
-__release__ = 20230130
+__version__ = '0.5.19'
+__release__ = 20230221
 __all__ = ['units', 'convert', 'Unit', 'is_Unit', 'set_unit', 'set_conversion', 'convertible', 'save']
 
 # import unyts.parameters
