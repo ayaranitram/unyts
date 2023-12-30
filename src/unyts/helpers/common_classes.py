9059ee85580195bc3989184c9f42408127851b9a
Martín Araya <martinaraya@gmail.com>
2023-12-30 22:35:26 +0100

    using typing Union

--- src/unyts/helpers/common_classes.py
+++ src/unyts/helpers/common_classes.py
@@ -6,37 +6,42 @@ Created on Sat Oct 24 14:34:59 2020
 @author: Martín Carlos Araya <martinaraya@gmail.com>
 """
 
-__version__ = '0.5.4'
-__release__ = 20230118
-__all__ = ['array_like', 'number', 'numeric', 'unit_or_str']
+__version__ = '0.5.5'
+__release__ = 20231230
+__all__ = ['array_like', 'number', 'numeric', 'unit_or_str', 'Empty', 'str_Empty']
+
+from typing import Union
+from ..unit_class import Unit
+from ..Empty import Empty, str_Empty
+
+unit_or_str = Union[str, Unit]
 
 try:
     from numpy import ndarray, int64, float64, int32, float32
+
     _numpy_ = True
 except ModuleNotFoundError:
     _numpy_ = False
 try:
     from pandas import Series, DataFrame
+
     _pandas_ = True
 except ModuleNotFoundError:
     _pandas_ = False
-from ..unit_class import Unit
-unit_or_str = (str, Unit)
-
 
 if _numpy_ and _pandas_:
-    number = (int, float, complex, int32, int64, float32, float64)
-    numeric = (int, float, complex, int32, int64, float32, float64, ndarray, Series, DataFrame)
-    array_like = (ndarray, Series, DataFrame)
+    number = Union[int, float, complex, int32, int64, float32, float64]
+    numeric = Union[int, float, complex, int32, int64, float32, float64, ndarray, Series, DataFrame]
+    array_like = Union[ndarray, Series, DataFrame]
 elif _numpy_:
-    number = (int, float, complex, int32, int64, float32, float64)
-    numeric = (int, float, complex, ndarray, int32, int64, float32, float64)
-    array_like = (ndarray,)
+    number = Union[int, float, complex, int32, int64, float32, float64]
+    numeric = Union[int, float, complex, ndarray, int32, int64, float32, float64]
+    array_like = ndarray
 elif _pandas_:
-    number = (int, float, complex)
-    numeric = (int, float, complex, Series, DataFrame)
-    array_like = (Series, DataFrame)
+    number = Union[int, float, complex]
+    numeric = Union[int, float, complex, Series, DataFrame]
+    array_like = Union[Series, DataFrame]
 else:
-    number = (int, float, complex)
-    numeric = (int, float, complex)
-    array_like = tuple()
+    number = Union[int, float, complex]
+    numeric = Union[int, float, complex]
+    array_like = None
