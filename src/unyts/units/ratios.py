0be0d06ee1d6bd9ef1966920d336d92c89b52f18
Martín Araya <martinaraya@gmail.com>
2024-11-24 02:29:14 +0100

    added TemperatureGradient as a unit class

--- src/unyts/units/ratios.py
+++ src/unyts/units/ratios.py
@@ -6,9 +6,9 @@ Created on Sat Oct 24 14:34:59 2020
 @author: Martín Carlos Araya <martinaraya@gmail.com>
 """
 
-__version__ = '0.5.30'
-__release__ = 20230724
-__all__ = ['Density', 'VolumeRatio', 'ProductivityIndex', 'PressureGradient']
+__version__ = '0.5.31'
+__release__ = 20241123
+__all__ = ['Density', 'VolumeRatio', 'ProductivityIndex', 'PressureGradient', 'TemperatureGradient']
 
 from ..dictionaries import dictionary as _dictionary
 from ..unit_class import Unit
@@ -55,5 +55,15 @@ class PressureGradient(Unit):
     def __init__(self, value: numeric, units: unit_or_str, name=None):
         self.name = 'pressure_gradient' if name is None else name
         self.kind = PressureGradient
+        self.value = self.check_value(value)
+        self.unit = self.check_unit(units)
+
+class TemperatureGradient(Unit):
+    class_units = _dictionary['TemperatureGradient']
+    __slots__ = ('unit', 'value', 'name', 'kind')
+
+    def __init__(self, value: numeric, units: unit_or_str, name=None):
+        self.name = 'temperature_gradient' if name is None else name
+        self.kind = TemperatureGradient
         self.value = self.check_value(value)
         self.unit = self.check_unit(units)
