0030b6c974ea869d2fbff113d6615d2ff227a4bb
Martín Araya <martinaraya@gmail.com>
2024-11-26 06:46:54 +0100

    pack 0.9.10

--- src/unyts/__init__.py
+++ src/unyts/__init__.py
@@ -7,7 +7,7 @@ Created on Sat Oct 24 18:24:20 2020
 """
 
 __version__ = '0.9.10'
-__release__ = 20241123
+__release__ = 20241124
 __all__ = ['units', 'convert', 'convertible', 'Unit', 'is_Unit', 'valid_unit',
            'set_unit', 'set_conversion', 'set_density', 'get_density'
            'save', 'start_gui', 'set_fvf', 'set_algorithm', 'set_parallel',
